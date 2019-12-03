d1b4b03afc6e8789c567376819efddc43ac5d9d4
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2019-12-03 18:30:19 -0500

    for loop for nav bar

--- js/index.js
+++ js/index.js
@@ -42,12 +42,17 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 let navBar = document.querySelectorAll("nav a");
-navBar[0].textContent = siteContent["nav"]["nav-item-1"]
-navBar[1].textContent = siteContent["nav"]["nav-item-2"]
-navBar[2].textContent = siteContent["nav"]["nav-item-3"]
-navBar[3].textContent = siteContent["nav"]["nav-item-4"]
-navBar[4].textContent = siteContent["nav"]["nav-item-5"]
-navBar[5].textContent = siteContent["nav"]["nav-item-6"]
+navBar.forEach((element, i) => {
+  element.textContent = siteContent["nav"][`nav-item-${i}`]
+})
+
+
+// navBar[0].textContent = siteContent["nav"]["nav-item-1"]
+// navBar[1].textContent = siteContent["nav"]["nav-item-2"]
+// navBar[2].textContent = siteContent["nav"]["nav-item-3"]
+// navBar[3].textContent = siteContent["nav"]["nav-item-4"]
+// navBar[4].textContent = siteContent["nav"]["nav-item-5"]
+// navBar[5].textContent = siteContent["nav"]["nav-item-6"]
 
 
 // creating two new elements to add to the navigation bar
